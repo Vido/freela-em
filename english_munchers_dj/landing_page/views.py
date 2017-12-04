ed938a3383f32bc55da25dac072407a7f7d1caab
Lucas Vido <vido@usp.br>
2017-12-04 12:17:24 -0200

    Subindo a IndexView

--- english_munchers_dj/landing_page/views.py
+++ english_munchers_dj/landing_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+
+    template_name = "landing_page/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
