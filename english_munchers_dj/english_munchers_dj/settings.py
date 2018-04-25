a21b13415179ee554c317cd9e0125e636c517659
Lucas Vido <vido@usp.br>
2018-04-25 10:45:57 -0300

    local settings

--- english_munchers_dj/english_munchers_dj/settings.py
+++ english_munchers_dj/english_munchers_dj/settings.py
@@ -132,3 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+try:
+    from .local_settings import *
+except ImportError as e:
+    print('[INFO] No local_settings was found.')
+    pass
