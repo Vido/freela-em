ee53fe1e1579781e47399c306d8427ebf7170611
Lucas Vido <vido@usp.br>
2018-05-15 11:15:55 -0300

    Paypal invoice json

--- english_munchers_dj/dashboard/views.py
+++ english_munchers_dj/dashboard/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.template import Template, Context
+from django.template.loader import render_to_string
 
 from landing_page.models import ClassInfo
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -18,3 +20,9 @@ class ClassInfoSendInvoice(LoginRequiredMixin, DetailView):
     template_name = 'dashboard/classinfo_sendinvoice.html'
     model = ClassInfo
 
+    def get_context_data(self, **kwargs):
+        context = super(ClassInfoSendInvoice, self).get_context_data(**kwargs)
+        invoice_json = render_to_string('paypal_integration/invoice.json', {})
+        print(invoice_json)
+        context['invoice_json'] = invoice_json
+        return context
--- english_munchers_dj/paypal_integration/views.py
+++ english_munchers_dj/paypal_integration/views.py
@@ -1,3 +1,16 @@
+from paypalrestsdk import Invoice
+import logging
+import json
+
 from django.shortcuts import render
 
-# Create your views here.
+#import sandbox
+
+def send_invoice(invoice_json):
+    logging.basicConfig(level=logging.INFO)
+    invoice = Invoice(invoice_json)
+    if invoice.create():
+        print(json.dumps(invoice.to_dict(), sort_keys=False, indent=4))
+    else:
+        print(invoice.error)
+
